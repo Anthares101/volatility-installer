6e614fdd175af9deb4812d96ada45de2dcf4e0cf
Ángel Heredia <33127189+anthares101@users.noreply.github.com>
2021-10-22 13:57:21 -0400

    Improve installer stability using virtual env

--- install_volatility.sh
+++ install_volatility.sh
@@ -26,42 +26,55 @@ function exitOnError {
     fi
 }
 
+# Elevate privileges
+if [[ $UID -ne 0 ]]; then
+   echo -e "\n${YELLOW}This script must be run as root${NC}"
+   exec sudo ORIGINAL_USER=$UID "$0" "$@"
+fi
+
 # Remove volatility dir if necesary
+INSTALLATION_DIR="/opt/vol"
 echo -ne "Removing previous volatility installations...\t"
-if [ -d "${HOME}/volatility" ]; then rm -Rf "${HOME}/volatility"; fi
+if [ -d $INSTALLATION_DIR ]; then rm -Rf $INSTALLATION_DIR; fi
+printCommandResult
+exitOnError
+
+# Install packages
+echo -ne "Installing packages...\t\t\t\t"
+apt install -y python2 git build-essential python3-virtualenv &> /dev/null
 printCommandResult
 exitOnError
 
 # Clone volatility
 echo -ne "Clone volatility to your home folder...\t\t"
-git clone "https://github.com/volatilityfoundation/volatility.git" "${HOME}/volatility" &> /dev/null
+git clone "https://github.com/volatilityfoundation/volatility.git" $INSTALLATION_DIR &> /dev/null
+printCommandResult
+exitOnError
+
+echo -ne "Preparing Python environment...\t\t\t"
+virtualenv -p python2 ${INSTALLATION_DIR}/venv &> /dev/null
 printCommandResult
 exitOnError
 
 # Install dependencies
 echo -ne "Installing dependencies...\t\t\t"
-python2 -m pip install distorm3==3.4.4 yara-python pycrypto pillow openpyxl ujson &> /dev/null
+${INSTALLATION_DIR}/venv/bin/pip install distorm3==3.4.4 yara-python pycrypto pillow openpyxl ujson &> /dev/null
 printCommandResult
 
-# Create bash script for execution using path
-echo -ne "Create bash script for execution...\t\t"
-echo -e "#! /bin/bash\n\npython2 $HOME/volatility/vol.py \"\$@\"" > "${HOME}/volatility/volatility2"
-chmod u+x "${HOME}/volatility/volatility2" &> /dev/null
-printCommandResult
-
-# Add to path
-echo -ne "Add volatility to path...\t\t\t"
-echo $PATH | grep -q  "$HOME/volatility"
-if [ $? -ne 0 ]; then
-	echo "PATH=\"\$HOME/volatility:\$PATH\"" >> "${HOME}/.bashrc"
-	if [ -f "${HOME}/.zshrc" ]; then
-		echo "PATH=\"\$HOME/volatility:\$PATH\"" >> "${HOME}/.zshrc"
-	fi
+# Add alias to path
+ALIAS_STR="""alias volatility='${INSTALLATION_DIR}/venv/bin/python /opt/vol/vol.py'"""
+USER_HOME_PATH="/home/$(id -un $ORIGINAL_USER)"
+echo -ne "Add alias for volatility to path...\t\t"
+# sed -i "/${ALIAS_STR}/d" $USER_HOME_PATH/.bashrc
+echo $ALIAS_STR >> $USER_HOME_PATH/.bashrc
+if [ -f "${HOME}/.zshrc" ]; then
+	# sed -i "/${ALIAS_STR}/d" $USER_HOME_PATH/.zshrc
+	echo $ALIAS_STR >> $USER_HOME_PATH/.zshrc
 fi
 printCommandResult
 
 if [ $STATUS -eq 0 ]; then
-	echo -e "\n${GREEN}All done! Enjoy the last volatility version!${NC}"
+	echo -e "\n${GREEN}All done! Enjoy the last volatility version! (Remember to restart the shell)${NC}"
 else
 	echo -e "\n${YELLOW}All done! Some errors during installation, maybe volatility dont work as expected${NC}"
 fi
